0c83d1e6452ca50b14e41b41e178a3f3395f4399
Francis <fgkiron@gmail.com>
2023-06-07 17:40:46 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    validate_triangle
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    raise TriangleError, "Invalid triangle: sides must be larger than 0" if [a, b, c].any? { |side| side <= 0 }
+    raise TriangleError, "Invalid triangle: triangle inequality violation" unless a + b > c && b + c > a && a + c > b
+  end
+
+  class TriangleError < StandardError
+  end
 end
